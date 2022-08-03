48f4fa02f7f6f9456e2005a40310e82ebdd48f35
evertonsil <everton.marques@uni9.edu.br>
2022-08-03 00:30:23 -0300

    v1

---
+++ logica_js/1-7_operacoes_booleanas.js
@@ -0,0 +1,35 @@
+/* 
+    Operaç]ões Booleanas | 1-7
+*/ 
+
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+// compara as variáveis numeroUm e stringUm
+if ( numeroUm == stringUm){
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+}
+else{
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+}
+
+// compara as variáveis numeroTrinta e stringTrinta
+if ( numeroTrinta == stringTrinta ){
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, mas tipos diferentes');
+}
+else{
+    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor');
+}
+
+// compara as variáveis numeroDez e stringDez
+if ( numeroDez == stringDez ){
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+}
+else{
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
+
