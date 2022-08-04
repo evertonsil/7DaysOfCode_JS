0bb8ab3432271ec0e0a1fd60c60e4d280705fc52
evertonsil <everton.marques@uni9.edu.br>
2022-08-04 08:53:24 -0300

    alterado organização das pastas e inicio do ex 2

---
+++ LogicaJS_2-7/2-7_variaveis.js
@@ -0,0 +1,7 @@
+/*
+    Variáveis | 2-7
+*/
+
+let nome = prompt ("Qual o seu nome?");
+let idade = prompt ("Quantos anos você tem?");
+let qualLInguagem = prompt ("Qual linguagem de programação você está estudando?");
