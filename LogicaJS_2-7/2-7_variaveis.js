cebbd160b64e1eb295fb5a839fe50bc1d7deef02
evertonsil <everton.marques@uni9.edu.br>
2022-08-05 08:31:04 -0300

    exercício 2 concluído

--- LogicaJS_2-7/2-7_variaveis.js
+++ LogicaJS_2-7/2-7_variaveis.js
@@ -6,9 +6,25 @@ let nome = prompt ("Qual o seu nome?");
 let idade = prompt ("Quantos anos você tem?");
 let qualLinguagem = prompt ("Qual linguagem de programação você está estudando?");
 
-let fraseResultado = ('Olá %s, você tem %s anos e já está aprendendo %s!', nome, idade, qualLinguagem);
+let fraseResultado = (`Olá ${nome}, você tem ${idade} anos e já está estudando ${qualLinguagem}!`);
 
 alert (fraseResultado);
+
+let voceGosta = prompt(`Ahh que legal! E você gosta de estudar ${qualLinguagem}? \nDigita 1 caso a resposta for SIM e 2 para NÃO`);
+
+if (voceGosta == 1){
+    mensagem = ("Muito bom! Continue estudando e você terá muito sucesso");
+}
+
+else if (voceGosta == 2){
+    mensagem = ("Ahh que pena... Já tentou aprender outras linguagens?");
+}
+
+else{
+    mensagem = ("Por favor digite uma opção válida.");
+}
+
+alert (mensagem);
 
 
 
